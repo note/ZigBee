29a520342be51f2fc6dcc5e3fa9048728d5d53d9
unknown <michal@.(none)>
2011-04-06 02:41:52 +0200

    Dziala wprowadzanie z "klawiatury"

--- src/lcd/lcd1.c
+++ src/lcd/lcd1.c
@@ -6,6 +6,9 @@
 	PORTC |= 8;\
 	PORTC &= 0xf7;\
 	_delay_ms(1);
+	
+#define SH_LEFT 0x10
+#define SH_RIGHT 0x14
 
 /** PORTC od najmlodszego bitu:
  * RS
@@ -19,8 +22,8 @@
  * itd. */
 
 void function_set(){
+	PORTC &= 0xfc; // RS=R/W=0
 	PORTD = 0x38; //interfejs 8bit, 2 wiersze, znaki 5x7
-	//PORTC &= 0xfe;
 	
 	TIC()
 }
@@ -33,9 +36,12 @@ void data_write(char data){
 	TIC()
 }
 
-void shift_cursor(){
-	PORTD = 0x14;
-	PORTC &= 0xfe;
+/**
+ *@param direction should be SH_LEFT or SH_RIGHT
+ */
+void shift_cursor(short direction){
+	PORTD = direction;
+	PORTC &= 0xfc; // RS=R/W=0
 	
 	TIC()
 }
@@ -65,7 +71,10 @@ void set_entry_mode(){
 int main(void){
 	//ustawienie portow
 	DDRD = 0xff;
-	DDRC = 0xf;
+	DDRC = 0x0f;
+	PORTC = 0x30;
+	DDRB = 0x00;
+	PORTB = 0x02;
 	
 	display_on();
 	
@@ -86,8 +95,40 @@ int main(void){
 	data_write('r');
 	data_write('l');
 	data_write('d');
+	data_write('a');
+	shift_cursor(SH_LEFT);
 	
-	PORTC |= 4;
+	short offset = 0;
+	
+	while(1){
+		if(!(PINC & 0x20)){
+			
+			++offset;
+			data_write('a'+offset);
+			shift_cursor(SH_LEFT);
+			
+			while(!(PINC & 0x20)){}
+			_delay_ms(50);
+		}
+		if(!(PINC & 0x10)){
+			
+			--offset;
+			data_write('a'+offset);
+			shift_cursor(SH_LEFT);
+			
+			while(!(PINC & 0x10)){}
+			_delay_ms(50);
+		}
+		if(!(PINB & 0x2)){
+			
+			shift_cursor(SH_RIGHT);
+			data_write('a');
+			shift_cursor(SH_LEFT);
+			offset = 0;
+			while(!(PINB & 0x2)){}
+			_delay_ms(50);
+		}
+	}
 	
 	/*while(1){
 		PORTC |= 4;
